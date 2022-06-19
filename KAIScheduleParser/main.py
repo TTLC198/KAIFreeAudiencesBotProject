5445c9d386567646267b6f80964e89ea6d12d8e3
Андрей Копанев <24122004@bk.ru>
2022-06-19 17:59:57 +0300

    new database 2

--- KAIScheduleParser/main.py
+++ KAIScheduleParser/main.py
@@ -1,6 +1,7 @@
 import json
 from datetime import timedelta, datetime
 import numpy as np
+import random
 
 import requests
 import sqlite3
@@ -20,11 +21,10 @@ def getConnectionString():
         return config['ConnectionStrings']['ScheduleConnectionSqlite'][12:]
 
 
-def getGroup(groupnum):
+def getGroup():
     params = dict(p_p_id='pubStudentSchedule_WAR_publicStudentSchedule10',
                   p_p_lifecycle='2',
-                  p_p_resource_id='getGroupsURL',
-                  query=groupnum)
+                  p_p_resource_id='getGroupsURL')
     response_json = requests.get(kaiUrl, params=params).json()
     groups = dict()
     for group in response_json:
@@ -45,16 +45,23 @@ def main():
     try:
         sqlite_connection = sqlite3.connect(getConnectionString())
         cursor = sqlite_connection.cursor()
-        cursor.execute("delete from groups")
-        for i in range(1, 10):
-            groups = getGroup(i)
-            for groupId in groups:
-                cursor.execute("insert into groups values (?, ?)", (groupId, groups[groupId]))
-                schedule = getScheduleById(groupId)
-                if len(schedule) != 0:
-                    for day in schedule:
-                        for lesson in day:
-                            
+        cursor.execute("delete from temp_time")
+        cursor.execute("select * from groups")
+        groups = cursor.fetchall()
+        random.seed()
+        random.shuffle(groups)
+        #groups = getGroup()
+        temp_time = set()
+        for groupId in groups[:100]:
+            #cursor.execute("insert into groups values (?, ?)", (groupId, groups[groupId]))
+            schedule = getScheduleById(groupId)
+            if len(schedule) != 0:
+                for day in schedule.values():
+                    for lesson in day:
+                        temp_t = normalizeString(lesson["dayDate"])
+                        if temp_t != '':
+                            temp_time.add((temp_t,))
+        cursor.executemany("insert into temp_time values (?)", temp_time)
         sqlite_connection.commit()
 
     except sqlite3.Error as error:
